dae35483a9b25852f27750f780d336c9c0ba5310
Kirill-Geskin <kirill.geskin@gmail.com>
2022-11-12 13:26:47 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ActionRPG_CPP
+
+Developed with Unreal Engine 4
---
+++ Source/ActionRPG_CPP/ActionRPG_CPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRPG_CPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONRPG_CPP_API AActionRPG_CPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

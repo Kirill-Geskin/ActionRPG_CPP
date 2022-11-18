0f2164f79fab730186e9a1c9eca5353019758a1f
Kirill-Geskin <kirill.geskin@gmail.com>
2022-11-18 20:31:42 +0400

    Added parent class for items

---
+++ Source/ActionRPG_CPP/Item.cpp
@@ -0,0 +1,51 @@
+
+#include "Item.h"
+#include "Components/SphereComponent.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionVolume = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionVolume"));
+	RootComponent = CollisionVolume;
+
+	CollisionVolume->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnOverlapBegin);
+	CollisionVolume->OnComponentEndOverlap.AddDynamic(this, &AItem::OnOverlapEnd);
+
+
+	//bRotate = false;
+	//RotationRate = 45.f;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	/*
+	if (bRotate)
+	{
+		FRotator Rotation = GetActorRotation();
+		Rotation.Yaw += DeltaTime * RotationRate;
+		SetActorRotation(Rotation);
+	}
+	*/
+}
+
+void AItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Super::OnOverlapBegin"));
+	//Destroy();
+}
+
+void AItem::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Super::OnOverlapEnd"));
+
+}
